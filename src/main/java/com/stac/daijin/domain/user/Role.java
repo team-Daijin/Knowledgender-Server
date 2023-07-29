27574632ced2f0d76ec4e82d4d152a3bb7f917c3
pea06 <101560095+pea06@users.noreply.github.com>
2023-07-29 13:21:32 +0900

    :card_file_box: :: perform user database

---
+++ src/main/java/com/stac/daijin/domain/user/Role.java
@@ -0,0 +1,14 @@
+package com.stac.daijin.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN("관리자"),
+    EXPORT("전문가"),
+    USER("사용자");
+
+    private final String value;
+}
---
+++ src/main/java/com/stac/daijin/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.stac.daijin.domain.user;
+
+import com.stac.daijin.global.jpa.BaseUUIDEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class User extends BaseUUIDEntity {
+    private String accountId;
+
+    private String password;
+
+    private String name;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}

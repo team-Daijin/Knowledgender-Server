f7faf04333762e08fd88aafc11bb531c3e2ca6f5
pea06 <101560095+pea06@users.noreply.github.com>
2023-07-29 13:23:15 +0900

    :card_file_box: :: add user database

---
+++ src/main/java/com/stac/daijin/domain/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.stac.daijin.domain.user.repository;
+
+import com.stac.daijin.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByAccountId(String accountId);
+
+    Boolean existsByAccountId(String accountId);
+}

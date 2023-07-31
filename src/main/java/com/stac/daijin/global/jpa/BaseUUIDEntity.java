4c7c4638870ba149030f2276701d3dc7ee7dc654
pea06 <101560095+pea06@users.noreply.github.com>
2023-07-31 22:54:08 +0900

    :zap: :: modify uuid generator

--- src/main/java/com/stac/daijin/global/jpa/BaseUUIDEntity.java
+++ src/main/java/com/stac/daijin/global/jpa/BaseUUIDEntity.java
@@ -1,16 +1,19 @@
 package com.stac.daijin.global.jpa;
 
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.UUID;
 
 @MappedSuperclass
-public class BaseUUIDEntity {
+@RequiredArgsConstructor
+public abstract class BaseUUIDEntity {
 
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)", nullable = false)
-    private UUID id;
+        @Id
+        @GeneratedValue(generator = "uuid2")
+        @GenericGenerator(name = "uuid2", strategy = "uuid2")
+        @Column(columnDefinition = "BINARY(16)")
+        protected UUID id;
+
 }

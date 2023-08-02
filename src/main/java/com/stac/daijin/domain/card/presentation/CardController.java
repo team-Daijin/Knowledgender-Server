3844f6524fb0a1f233f64ebc23876527dbb5d5ef
pea06 <101560095+pea06@users.noreply.github.com>
2023-08-02 23:41:32 +0900

    :sparkles: :: add card apis

--- src/main/java/com/stac/daijin/domain/card/presentation/CardController.java
+++ src/main/java/com/stac/daijin/domain/card/presentation/CardController.java
@@ -1,8 +1,12 @@
 package com.stac.daijin.domain.card.presentation;
 
-import com.stac.daijin.domain.card.presentation.dto.SaveCardRequest;
+import com.stac.daijin.domain.card.CardCategory;
+import com.stac.daijin.domain.card.presentation.dto.request.SaveCardRequest;
+import com.stac.daijin.domain.card.presentation.dto.request.UpdateCardRequest;
 import com.stac.daijin.domain.card.service.DeleteCardService;
+import com.stac.daijin.domain.card.service.QueryAllCardByCategoryService;
 import com.stac.daijin.domain.card.service.SaveCardService;
+import com.stac.daijin.domain.card.service.UpdateCardService;
 import com.stac.daijin.global.annotation.AuthRequired;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +21,8 @@ public class CardController {
 
     private final SaveCardService saveCardService;
     private final DeleteCardService deleteCardService;
+    private final UpdateCardService updateCardService;
+    private final QueryAllCardByCategoryService queryAllCardByCategoryService;
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.OK)
@@ -28,6 +34,33 @@ public class CardController {
         saveCardService.execute(request, user);
     }
 
+    @GetMapping("/{category}")
+    @ResponseStatus(HttpStatus.OK)
+    public void getCardByCategory(
+            @PathVariable CardCategory category
+    ) {
+        queryAllCardByCategoryService.execute(category);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void getCardById(
+            @PathVariable UUID id
+    ) {
+
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @AuthRequired
+    public void updateCard(
+            @PathVariable UUID id,
+            @ModelAttribute UpdateCardRequest request,
+            @RequestAttribute String user
+    ) {
+        updateCardService.execute(id, request, user);
+    }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @AuthRequired
@@ -37,5 +70,4 @@ public class CardController {
     ) {
         deleteCardService.execute(id, user);
     }
-
 }

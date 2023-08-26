bcd3a8cf07bdcfff05f1ad6473138612cd481c16
pea06 <101560095+pea06@users.noreply.github.com>
2023-08-26 23:55:48 +0900

    :necktie: :: 메세지 보낼때 소켓 클라이언트에서 유저 가져오게 수정

--- src/main/java/com/stac/daijin/domain/chat/presentation/ChatController.java
+++ src/main/java/com/stac/daijin/domain/chat/presentation/ChatController.java
@@ -3,25 +3,33 @@ package com.stac.daijin.domain.chat.presentation;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.annotation.OnEvent;
 import com.stac.daijin.domain.chat.presentation.dto.request.MessageRequest;
+import com.stac.daijin.domain.chat.presentation.dto.response.MessageListResponse;
+import com.stac.daijin.domain.chat.service.QueryMessageListService;
 import com.stac.daijin.domain.chat.service.SendMessageService;
 import com.stac.daijin.global.socket.config.property.SocketEventProperty;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/chat/{roomId}")
+@RequestMapping("/api/chat/{roomId}")
 @RequiredArgsConstructor
 public class ChatController {
     private final SendMessageService sendMessageService;
+    private final QueryMessageListService queryMessageListService;
 
     @OnEvent(SocketEventProperty.MESSAGE_KEY)
     public void sendMessage(
             SocketIOClient client,
-            MessageRequest request,
-            @RequestAttribute String user
+            MessageRequest request
     ) {
-        sendMessageService.sendMessage(request, user);
+        sendMessageService.sendMessage(request, client);
+    }
+
+    @GetMapping
+    public MessageListResponse getMessageList(
+            @PathVariable String roomId,
+            @RequestParam("page") int page
+    ) {
+        return queryMessageListService.execute(roomId, page);
     }
 }
--- src/main/java/com/stac/daijin/domain/chat/service/SendMessageService.java
+++ src/main/java/com/stac/daijin/domain/chat/service/SendMessageService.java
@@ -1,5 +1,6 @@
 package com.stac.daijin.domain.chat.service;
 
+import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.stac.daijin.domain.chat.Message;
 import com.stac.daijin.domain.chat.Room;
@@ -7,27 +8,33 @@ import com.stac.daijin.domain.chat.facade.RoomFacade;
 import com.stac.daijin.domain.chat.presentation.dto.request.MessageRequest;
 import com.stac.daijin.domain.chat.presentation.dto.response.MessageResponse;
 import com.stac.daijin.domain.chat.repository.MessageRepository;
+import com.stac.daijin.domain.user.User;
+import com.stac.daijin.domain.user.facade.UserFacade;
 import com.stac.daijin.global.socket.config.property.SocketEventProperty;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SendMessageService {
     private final SocketIOServer socketIOServer;
     private final MessageRepository messageRepository;
     private final RoomFacade roomFacade;
+    private final UserFacade userFacade;
 
     @Transactional
-    public void sendMessage(MessageRequest request, String accountId) {
+    public void sendMessage(MessageRequest request, SocketIOClient client) {
+        User user = userFacade.findUserByClient(client);
         Room room = roomFacade.getRoomByRoomId(request.getRoomId());
         Message message = messageRepository.save(
                 Message.builder()
                         .message(request.getMessage())
                         .roomId(request.getRoomId())
-                        .accountId(accountId)
+                        .accountId(user.getAccountId())
                         .build()
         );
         socketIOServer.getRoomOperations(request.getRoomId())

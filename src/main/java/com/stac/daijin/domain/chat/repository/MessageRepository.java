0af8a100e72b09e532e47dc34866d58bb2e8ac5c
pea06 <101560095+pea06@users.noreply.github.com>
2023-08-26 23:57:42 +0900

    :necktie: :: get message list logic

--- src/main/java/com/stac/daijin/domain/chat/repository/MessageRepository.java
+++ src/main/java/com/stac/daijin/domain/chat/repository/MessageRepository.java
@@ -1,10 +1,17 @@
 package com.stac.daijin.domain.chat.repository;
 
 import com.stac.daijin.domain.chat.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @EnableMongoRepositories
 public interface MessageRepository extends MongoRepository<Message, String> {
+
+    Page<Message> findAllByRoomId(String roomId, Pageable pageable);
 }
---
+++ src/main/java/com/stac/daijin/domain/chat/service/QueryMessageListService.java
@@ -0,0 +1,36 @@
+package com.stac.daijin.domain.chat.service;
+
+import com.stac.daijin.domain.chat.facade.RoomFacade;
+import com.stac.daijin.domain.chat.presentation.dto.response.MessageListResponse;
+import com.stac.daijin.domain.chat.presentation.dto.response.MessageResponse;
+import com.stac.daijin.domain.chat.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryMessageListService {
+
+    private final MessageRepository messageRepository;
+    private final RoomFacade roomFacade;
+
+    public MessageListResponse execute(
+            String roomId,
+            int page
+    ) {
+        Pageable pageable = PageRequest.of(page, 10, Sort.by("sentAt"));
+        return new MessageListResponse(
+                messageRepository.findAllByRoomId(roomFacade.getRoomByRoomId(roomId).getId(), pageable)
+                        .stream().map(
+                                m -> MessageResponse.of(
+                                        m, roomId
+                                )
+                        ).collect(Collectors.toList())
+        );
+    }
+}

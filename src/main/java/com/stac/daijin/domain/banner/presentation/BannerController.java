8144110236748a24a6dbcab6829d7d10a47417b6
pea06 <101560095+pea06@users.noreply.github.com>
2023-09-01 23:32:47 +0900

    :sparkles: :: add get banner list api

---
+++ src/main/java/com/stac/daijin/domain/banner/presentation/BannerController.java
@@ -0,0 +1,25 @@
+package com.stac.daijin.domain.banner.presentation;
+
+import com.stac.daijin.domain.banner.presentation.dto.response.BannerListResponse;
+import com.stac.daijin.domain.banner.service.QueryBannerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/banner")
+@RequiredArgsConstructor
+public class BannerController {
+
+    private final QueryBannerService queryBannerService;
+
+    @GetMapping("/")
+    public BannerListResponse getBannerList(
+
+    ) {
+        return queryBannerService.execute();
+    }
+
+
+}

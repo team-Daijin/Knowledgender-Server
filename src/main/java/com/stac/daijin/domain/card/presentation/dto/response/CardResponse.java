d8533c7311d36d903c97cb5654e71a39ec3cf34a
pea06 <101560095+pea06@users.noreply.github.com>
2023-09-02 20:49:25 +0900

    :zap: :: add card response of

--- src/main/java/com/stac/daijin/domain/card/presentation/dto/response/CardResponse.java
+++ src/main/java/com/stac/daijin/domain/card/presentation/dto/response/CardResponse.java
@@ -1,13 +1,16 @@
 package com.stac.daijin.domain.card.presentation.dto.response;
 
+import com.stac.daijin.domain.card.Card;
 import com.stac.daijin.domain.card.enums.CardCategory;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class CardResponse {
     private UUID id;
     private String title;
@@ -15,4 +18,15 @@ public class CardResponse {
     private String category;
     private String content;
     private String image;
+
+    public static CardResponse of(Card card) {
+        return CardResponse.builder()
+                .id(card.getId())
+                .title(card.getTitle())
+                .writer(card.getUser().getAccountId())
+                .category(card.getCategory())
+                .content(card.getContent())
+                .image(card.getImage())
+                .build();
+    }
 }

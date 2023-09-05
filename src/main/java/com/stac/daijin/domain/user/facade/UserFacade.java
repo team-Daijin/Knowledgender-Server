7cf19214ccaba27fac28f30a6b8cc1cf0d04a111
pea06 <101560095+pea06@users.noreply.github.com>
2023-09-05 09:16:27 +0900

    :zap: :: add get current user

--- src/main/java/com/stac/daijin/domain/user/facade/UserFacade.java
+++ src/main/java/com/stac/daijin/domain/user/facade/UserFacade.java
@@ -4,8 +4,10 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.stac.daijin.domain.user.User;
 import com.stac.daijin.domain.user.exception.UserNotFoundException;
 import com.stac.daijin.domain.user.repository.UserRepository;
+import com.stac.daijin.global.security.auth.AuthUser;
 import com.stac.daijin.global.socket.config.property.SocketStoreKey;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,10 @@ import java.util.UUID;
 public class UserFacade {
 
     private final UserRepository userRepository;
-
+    public User getCurrentUser() {
+        AuthUser auth = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return auth.getUser();
+    }
     @Transactional(readOnly = true)
     public User findUserByAccountId(String accountId) {
         return userRepository.findByAccountId(accountId)

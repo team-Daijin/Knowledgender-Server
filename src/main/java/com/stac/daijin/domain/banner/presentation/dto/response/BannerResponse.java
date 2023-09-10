bcef815bf52be07308aa72bc2852e30f8841a362
pea06 <101560095+pea06@users.noreply.github.com>
2023-09-10 22:02:36 +0900

    :bug: :: response에 id 넣는거 까먹음

--- src/main/java/com/stac/daijin/domain/banner/presentation/dto/response/BannerResponse.java
+++ src/main/java/com/stac/daijin/domain/banner/presentation/dto/response/BannerResponse.java
@@ -4,9 +4,12 @@ import com.stac.daijin.domain.banner.Banner;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.UUID;
+
 @Getter
 @Builder
 public class BannerResponse {
+    private UUID id;
     private String fileUrl;
     private String redirect;
 

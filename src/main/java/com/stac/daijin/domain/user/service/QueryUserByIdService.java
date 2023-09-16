ef57373bb42bdf502147df80979e4801925a9f64
pea06 <101560095+pea06@users.noreply.github.com>
2023-09-16 22:01:40 +0900

    :zap: :: 사용자의 성별이 존재하지 않을때 공백 처리

--- src/main/java/com/stac/daijin/domain/user/service/QueryUserByIdService.java
+++ src/main/java/com/stac/daijin/domain/user/service/QueryUserByIdService.java
@@ -1,6 +1,7 @@
 package com.stac.daijin.domain.user.service;
 
 import com.stac.daijin.domain.user.User;
+import com.stac.daijin.domain.user.enums.Gender;
 import com.stac.daijin.domain.user.facade.UserFacade;
 import com.stac.daijin.domain.user.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,7 @@ public class QueryUserByIdService {
     public UserResponse execute() {
         User user = userFacade.getCurrentUser();
         return new UserResponse(
-                user.getName(), user.getAge(), user.getGender().getGender()
+                user.getName(), user.getAge(), user.getGender() == null ? "" : user.getGender().getGender()
         );
     }
 
